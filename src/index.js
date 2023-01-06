da1df12af8bfefeb09e273e94204f272f4d24a74
Bohdan <nilsrondhuis@ukr.net>
2023-01-06 23:42:29 +0200

    add base name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-05-movies">
     <React.StrictMode>
       <App />
     </React.StrictMode>
